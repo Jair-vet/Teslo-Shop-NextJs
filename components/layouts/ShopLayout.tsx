8a1a468ff06251499944558d62beea3ed85f198d
Jair <jairaceves56@gmail.com>
2023-05-01 13:25:12 -0600

    Creamos el Layout de la pagina Shop

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,48 @@
+import Head from "next/head"
+import { PropsWithChildren, FC } from "react"
+
+interface Props extends PropsWithChildren {
+    title: string
+    pageDescription: string
+    imageFullUrl?: string
+}
+
+export const ShopLayout:FC<Props> = ({ children, imageFullUrl, pageDescription, title}) => {
+  return (
+    <>
+        <Head>
+            <title>{ title }</title>
+
+            <meta name="description" content={ pageDescription }/>
+            <meta name="og:title" content={ title }/>
+            <meta name="og:description" content={ pageDescription }/>
+
+            {
+                imageFullUrl && (
+                    <meta name="og:image" content={ imageFullUrl }/>
+                )
+            }
+        </Head>
+
+        <nav>
+            {/* { TODO: Navbar } */}
+        </nav>
+
+        {/* TODO: Sidebar */}
+
+        <main style={{
+            margin: '80px auto',
+            maxWidth: '1440px',
+            padding: '0px 30px'
+        }}>
+            { children }
+        </main>
+
+
+        {/* FOOTER */}
+        <footer>
+            {/* { TODO: Footer } */}
+        </footer>
+    </>
+  )
+}
