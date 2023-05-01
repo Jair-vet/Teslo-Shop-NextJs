3c481a927d89e5eb0edc9ff72ce8e2a9a3d00a74
Jair <jairaceves56@gmail.com>
2023-05-01 14:50:30 -0600

    Cargamos las imagenes de la data que estructuramos de manera statica

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { ShopLayout } from '@/components/layouts'
-import { Typography } from '@mui/material'
+import { initialData } from '@/database/products'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
 import { Inter } from 'next/font/google'
 
 
@@ -10,6 +11,26 @@ export default function Home() {
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos aqui en Teslo'}>
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map( product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia 
+                    component='img'
+                    image={ `products/${ product.images[0] }` }
+                    alt={ product.title }
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
+
     </ShopLayout>
   )
 }
