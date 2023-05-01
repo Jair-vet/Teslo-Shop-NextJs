e07a22f741e9098229e842d54270c50441bfb705
Jair <jairaceves56@gmail.com>
2023-05-01 13:25:39 -0600

    Agregamos el Layout de Shop al index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { ShopLayout } from '@/components/layouts'
+import { Typography } from '@mui/material'
 import { Inter } from 'next/font/google'
 
 
@@ -5,8 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <h1>Teslo App</h1>
-    </>
+    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos aqui en Teslo'}>
+      <Typography variant='h1' component='h1'>Tienda</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
