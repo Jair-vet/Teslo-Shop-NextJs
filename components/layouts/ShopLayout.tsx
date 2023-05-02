ebd92f13e8d17fde978c28d59866c9fb374b9c70
Jair <jairaceves56@gmail.com>
2023-05-02 13:53:46 -0600

    Agregamos el SideMenu creado

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head"
 import { PropsWithChildren, FC } from "react"
-import { Navbar } from "../ui"
+import { Navbar, SideMenu } from "../ui"
 
 interface Props extends PropsWithChildren {
     title: string
@@ -25,12 +25,13 @@ export const ShopLayout:FC<Props> = ({ children, imageFullUrl, pageDescription, 
             }
         </Head>
 
+        {/* {Navbar } */}
         <nav>
-            {/* { TODO: Navbar } */}
             <Navbar />
         </nav>
 
-        {/* TODO: Sidebar */}
+        {/* Sidebar */}
+        <SideMenu />
 
         <main style={{
             margin: '80px auto',
