1cdcb22cb4546d317bf39338c06ea32921e809a0
Jair <jairaceves56@gmail.com>
2023-05-02 17:26:10 -0600

    Agregamos una segunda imagen al hacer hover sobre la tarjeta

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,6 +1,6 @@
 import { IProduct } from "@/interfaces"
-import { Card, CardActionArea, CardMedia, Grid } from "@mui/material"
-import { FC } from "react"
+import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from "@mui/material"
+import { FC, useMemo, useState } from "react"
 
 
 interface Props {
@@ -8,17 +8,38 @@ interface Props {
 }
 
 export const ProductCard: FC<Props> = ({ product }) => {
-  return (
-    <Grid item xs={6} sm={4} key={product.slug}>
-        <Card>
-        <CardActionArea>
-            <CardMedia 
-            component='img'
-            image={ `products/${ product.images[0] }` }
-            alt={ product.title }
-            />
-        </CardActionArea>
-        </Card>
-    </Grid>
+
+    const [isHovered, setIsHovered] = useState(false)
+
+    const productImage = useMemo(() => {
+        return isHovered
+        ? `products/${ product.images[1] }`
+        : `products/${ product.images[0] }`
+    }, [isHovered, product.images])
+
+    return (
+        <Grid 
+            item
+            xs={6}
+            sm={4}
+            onMouseEnter={ () => setIsHovered(true) }
+            onMouseLeave={ () => setIsHovered(false) }
+        >
+            <Card>
+            <CardActionArea>
+                <CardMedia 
+                    className="fadeIn"
+                    component='img'
+                    image={ productImage }
+                    alt={ product.title }
+                />
+            </CardActionArea>
+            </Card>
+
+            <Box sx={{ mt: 1 }} className="fadeIn">
+                <Typography fontWeight={700}>{ product.title }</Typography>
+                <Typography fontWeight={500}>${ product.price }</Typography>
+            </Box>
+        </Grid>
   )
 }
