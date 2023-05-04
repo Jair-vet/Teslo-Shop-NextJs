d4d79d0275e9311fa69ccd94167632096649eb3c
Jair <jairaceves56@gmail.com>
2023-05-03 18:11:37 -0600

    Agregamos la lista de productos

---
+++ pages/cart/index.tsx
@@ -0,0 +1,44 @@
+import { CartList } from "@/components/cart"
+import { ShopLayout } from "@/components/layouts"
+import { Card, CardContent, Grid, Typography, Divider, Box, Button } from "@mui/material"
+
+const CartPage = () => (
+    <ShopLayout title={"Carrito - 3"} pageDescription={"Carrito de compras de la Tienda"}>
+        <Typography variant="h1" component="h1">Carrito</Typography>
+
+        <Grid container>
+            {/* CardList */}
+            <Grid item xs={12} sm={7}>
+                <CartList editable/>
+            </Grid>
+
+            {/* Datos Compra */}
+            <Grid item xs={12} sm={5} sx={{ mt: 8 }}>
+                <Card className='summary-card'>
+                    <CardContent>
+                        <Typography
+                            variant="h2"
+                            style={{
+                                textAlign: 'center',
+                                textTransform: 'uppercase',
+                                fontWeight: 'bold'
+                            }}
+                        >Orden</Typography>
+                        <Divider sx={{ my: 1 }} />
+
+                        {/* Orden Summary */}
+
+                        <Box sx={{ mt: 3 }}>
+                            <Button color="secondary" className='circular-btn' fullWidth>
+                                Checkout
+                            </Button>
+                        </Box>
+
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
+    </ShopLayout>
+)
+
+export default CartPage
