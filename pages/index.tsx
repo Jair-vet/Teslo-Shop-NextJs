698cde2a06138c634e068f1f0556bb86bd61a849
Jair <jairaceves56@gmail.com>
2023-05-10 13:44:57 -0600

    Creamos la peticion usando SWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,28 @@
 import { ShopLayout } from '@/components/layouts'
 import { ProductList } from '@/components/products'
-import { initialData } from '@/database/products'
 import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
 import { Inter } from 'next/font/google'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+import useSWR from 'swr'
+const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
+
+export default function HomePage() {
+
+  const { data, error, isLoading } = useSWR('/api/products', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos aqui en Teslo'}>
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
       <ProductList 
-        products={ initialData.products as any }        
+        products={ data }        
       />
 
     </ShopLayout>
