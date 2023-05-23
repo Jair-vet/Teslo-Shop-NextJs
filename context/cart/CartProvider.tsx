dae88070b54808b0a79078eee839211b4ec9267b
Jair <jairaceves56@gmail.com>
2023-05-23 13:24:16 -0600

    Creamos el CART_INITIAL_STATE y le agregamos los valores por default

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -6,11 +6,25 @@ import { CartContext, cartReducer } from './';
 
 export interface CartState {
     cart: ICartProduct[];
+    numberOfItems: number;
+    subTotal: number;
+    tax: number;
+    total: number;
 }
 
+
 const CART_INITIAL_STATE: CartState = {
-    cart:  Cookie.get('cart') ? JSON.parse(Cookie.get('cart')!) : []
+    cart: [],
+    numberOfItems: 0,
+    subTotal: 0,
+    tax: 0,
+    total: 0,
 }
+
+
+// const CART_INITIAL_STATE: CartState = {
+//     cart:  Cookie.get('cart') ? JSON.parse(Cookie.get('cart')!) : []
+// }
 
 interface Props {
     children: ReactNode
@@ -30,9 +44,29 @@ export const CartProvider:FC<Props> = ({ children }) => {
         }
     }, []);
 
+    //  Guardad la informacion en las cookies
     useEffect(() => {
         Cookie.set('cart', JSON.stringify( state.cart ));
     }, [state.cart]);
+
+    
+    //  Modificar el Orden Summary
+    useEffect(() => {
+        
+        const numberOfItems = state.cart.reduce( ( prev, current ) => current.quantity + prev , 0 );
+        const subTotal = state.cart.reduce( ( prev, current ) => (current.price * current.quantity) + prev, 0 );
+        const taxRate =  Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
+    
+        const orderSummary = {
+            numberOfItems,
+            subTotal,
+            tax: subTotal * taxRate,
+            total: subTotal * ( taxRate + 1 )
+        }
+        
+        dispatch({ type: '[Cart] - Update order summary', payload: orderSummary });
+    }, [state.cart]);
+
 
     const addProductToCart = ( product: ICartProduct ) => {
         //! Nivel 1
