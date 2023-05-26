6c6b70e80189ac8d374bf2478ee952465ad40128
Jair <jairaceves56@gmail.com>
2023-05-26 13:48:37 -0600

    Usamos React Hook Forms para validar los formularios

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,40 +1,74 @@
 import NextLink from 'next/link';
 import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '@/components/layouts';
+import { useForm } from 'react-hook-form';
+
+
+type FormData = {
+    email   : string,
+    password: string,
+};
 
 
 const LoginPage = () => {
-  return (
-    <AuthLayout title={'Ingresar'}>
-        <Box sx={{ width: 350, padding:'10px 20px' }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Typography variant='h1' component="h1">Iniciar Sesión</Typography>
-                </Grid>
 
-                <Grid item xs={12}>
-                    <TextField label="Correo" variant="filled" fullWidth />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField label="Contraseña" type='password' variant="filled" fullWidth />
-                </Grid>
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
-                <Grid item xs={12}>
-                    <Button color="secondary" className='circular-btn' size='large' fullWidth>
-                        Ingresar
-                    </Button>
-                </Grid>
+    const onLoginUser = ( data: FormData ) => {
+        console.log({data});
+        
+    }
 
-                <Grid item xs={12} display='flex' justifyContent='end'>
-                    <NextLink href="/auth/register" passHref legacyBehavior>
-                        <Link>
-                            ¿No tienes cuenta?
-                        </Link>
-                    </NextLink>
-                </Grid>
-            </Grid>
-        </Box>
-    </AuthLayout>
+    return (
+        <AuthLayout title={'Ingresar'}>
+            <form onSubmit={ handleSubmit(onLoginUser) }>
+                <Box sx={{ width: 350, padding:'10px 20px' }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Typography variant='h1' component="h1">Iniciar Sesión</Typography>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField 
+                                label="Correo" 
+                                variant="filled" 
+                                fullWidth 
+                                { ...register('email') }
+                                />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField 
+                                label="Contraseña" 
+                                type='password' 
+                                variant="filled" 
+                                fullWidth 
+                                { ...register('password') }
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Button 
+                                type="submit"
+                                color="secondary" 
+                                className='circular-btn' 
+                                size='large' 
+                                fullWidth
+                            >
+                                Ingresar
+                            </Button>
+                        </Grid>
+
+                        <Grid item xs={12} display='flex' justifyContent='end'>
+                            <NextLink href="/auth/register" passHref legacyBehavior>
+                                <Link>
+                                    ¿No tienes cuenta?
+                                </Link>
+                            </NextLink>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </form>
+        </AuthLayout>
   )
 }
 
