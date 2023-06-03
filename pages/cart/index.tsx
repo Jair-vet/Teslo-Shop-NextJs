3f9558bef3cdc9736d5a59fcd535612d37baea9f
Jair <jairaceves56@gmail.com>
2023-06-02 18:55:37 -0600

    Mostramos la pagina del carrito vacio condicionalmente

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,45 +1,59 @@
 import { CartList, OrderSummary } from "@/components/cart"
 import { ShopLayout } from "@/components/layouts"
+import { CartContext } from "@/context";
 import { Card, CardContent, Grid, Typography, Divider, Box, Button } from "@mui/material"
+import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
 
-const CartPage = () => (
-    <ShopLayout title={"Carrito - 3"} pageDescription={"Carrito de compras de la Tienda"}>
-        <Typography variant="h1" component="h1">Carrito</Typography>
+const CartPage = () => {
 
-        <Grid container>
-            {/* CardList */}
-            <Grid item xs={12} sm={7}>
-                <CartList editable/>
+    const { isLoaded, numberOfItems } = useContext( CartContext );
+    const router = useRouter();
+
+    useEffect(() => {
+        if (isLoaded && !numberOfItems)
+            router.replace('/cart/empty');
+    }, [isLoaded, numberOfItems, router]);
+ 
+    // utilizando numberOfItems
+    if (!numberOfItems)
+        return null;
+
+    return (
+        <ShopLayout title='Carrito - 3' pageDescription={'Carrito de compras de la tienda'}>
+            <Typography variant='h1' component='h1'>Carrito</Typography>
+
+            <Grid container>
+                <Grid item xs={ 12 } sm={ 7 }>
+                    <CartList editable />
+                </Grid>
+                <Grid item xs={ 12 } sm={ 5 }>
+                    <Card className='summary-card'>
+                        <CardContent>
+                            <Typography variant='h2'>Orden</Typography>
+                            <Divider sx={{ my:1 }} />
+
+                            <OrderSummary />
+
+                            <Box sx={{ mt: 3 }}>
+                                <Button 
+                                    color="secondary"
+                                    className='circular-btn' 
+                                    fullWidth
+                                    href='/checkout/address'
+                                >
+                                    Checkout
+                                </Button>
+                            </Box>
+
+                        </CardContent>
+                    </Card>
+                </Grid>
             </Grid>
 
-            {/* Datos Compra */}
-            <Grid item xs={12} sm={5} sx={{ mt: 8 }}>
-                <Card className='summary-card'>
-                    <CardContent>
-                        <Typography
-                            variant="h2"
-                            style={{
-                                textAlign: 'center',
-                                textTransform: 'uppercase',
-                                fontWeight: 'bold'
-                            }}
-                        >Orden</Typography>
-                        <Divider sx={{ my: 1 }} />
 
-                        {/* Orden Summary */}
-                        <OrderSummary />
-
-                        <Box sx={{ mt: 3 }}>
-                            <Button color="secondary" className='circular-btn' fullWidth>
-                                Checkout
-                            </Button>
-                        </Box>
-
-                    </CardContent>
-                </Card>
-            </Grid>
-        </Grid>
-    </ShopLayout>
-)
+        </ShopLayout>
+    )
+}
 
 export default CartPage
