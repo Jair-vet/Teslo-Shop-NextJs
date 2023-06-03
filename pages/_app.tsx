4f40a267a5aebab8816f9b721abf8ca795431a07
Jair <jairaceves56@gmail.com>
2023-06-03 14:49:12 -0600

    Envolvemos nuetra app con el SessionProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AuthProvider, CartProvider, UiProvider } from '@/context'
 import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react"
 import { lightTheme } from '@/themes'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
@@ -7,21 +8,23 @@ import { SWRConfig } from 'swr'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig 
-      value={{
-        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-      }}
-    >
-      <AuthProvider >
-        <CartProvider>
-          <UiProvider>
-            <ThemeProvider theme={ lightTheme }>
-              <CssBaseline />
-              <Component {...pageProps} />
-            </ThemeProvider>
-          </UiProvider>
-        </CartProvider>
-      </AuthProvider>
-    </SWRConfig>
+    <SessionProvider>
+      <SWRConfig 
+        value={{
+          fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+        }}
+      >
+        <AuthProvider >
+          <CartProvider>
+            <UiProvider>
+              <ThemeProvider theme={ lightTheme }>
+                <CssBaseline />
+                <Component {...pageProps} />
+              </ThemeProvider>
+            </UiProvider>
+          </CartProvider>
+        </AuthProvider>
+      </SWRConfig>
+    </SessionProvider>
   )
 }
