ed9c2c57e96bd284cdbc6621f56481c87af5f6ff
Jair <jairaceves56@gmail.com>
2023-06-04 14:05:29 -0600

    Reestablecemos los valores del useForm

--- pages/checkout/address.tsx
+++ pages/checkout/address.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
 import Cookies from 'js-cookie';
@@ -42,9 +42,23 @@ const AddressPage = () => {
     const router = useRouter();
     const { updateAddress} = useContext( CartContext );
 
-    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
-       defaultValues: getAddressFromCookies() 
-    });
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>({
+        defaultValues: {
+             firstName: '',
+             lastName: '',
+             address: '',
+             address2: '',
+             zip: '',
+             city: '',
+             country: countries[0].code,
+             phone: '',
+        } 
+     });
+ 
+     useEffect(() => {
+         reset(getAddressFromCookies() );
+ 
+     }, [reset])
 
     const onSubmitAddress = ( data: FormData ) => {
         updateAddress( data );
