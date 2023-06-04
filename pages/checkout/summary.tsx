ed1e32488147e5dbf517d40d54cadf3c087dd4c6
Jair <jairaceves56@gmail.com>
2023-06-04 14:06:15 -0600

    Implementamos que debemos tener algo en el shippingAddress, de lo contraio a loguearse

--- pages/checkout/summary.tsx
+++ pages/checkout/summary.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import NextLink from 'next/link';
 
 import { Link, Box, Button, Card, CardContent, Divider, Grid, Typography } from '@mui/material';
@@ -7,11 +7,23 @@ import { CartContext } from '../../context';
 import { ShopLayout } from '../../components/layouts/ShopLayout';
 import { CartList, OrderSummary } from '../../components/cart';
 import { countries } from '../../utils';
+import { useRouter } from 'next/router';
+import Cookies from 'js-cookie';
 
 
 const SummaryPage = () => {
 
+    const router = useRouter();
     const { shippingAddress, numberOfItems } = useContext( CartContext );
+    
+    useEffect(() => {
+        if ( !Cookies.get('firstName') ) {
+            router.push('/checkout/address');
+        }
+    }, [ router ]);
+    
+
+
     if ( !shippingAddress ) {
         return <></>;
     }
