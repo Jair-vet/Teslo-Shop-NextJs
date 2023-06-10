b0e920ca241e241aef329f6330b92ef0f8fb3403
Jair <jairaceves56@gmail.com>
2023-06-10 11:20:45 -0600

    Creamos la base para guardar las Ordenes en la DataBase

--- database/index.ts
+++ database/index.ts
@@ -1,10 +1,10 @@
-
 export * as db from './db';
 export * as dbProducts from './dbProducts';
+export * as dbOrders from './dbOrders';
 export * as dbUsers from './dbUsers';
 export * from './constants';
 
 
-// Base de Datos de Prueba
-export * as seedDatabase from './seed-data'
 
+// Base de datos de prueba
+export * as seedDatabase from './seed-data';
