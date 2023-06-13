1df55fbbd4a41f085c32267b57b70ee0d5d63fc8
Jair <jairaceves56@gmail.com>
2023-06-13 09:58:48 -0600

    Agregamos el ambiente para usar Paypal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,36 @@
-import { AuthProvider, CartProvider, UiProvider } from '@/context'
 import '@/styles/globals.css'
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import { SessionProvider } from "next-auth/react"
-import { lightTheme } from '@/themes'
-import { CssBaseline, ThemeProvider } from '@mui/material'
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app'
-import { SWRConfig } from 'swr'
+import { lightTheme } from '../themes';
+import { SWRConfig } from 'swr';
+import { AuthProvider, CartProvider, UiProvider } from '@/context';
 
 export default function App({ Component, pageProps }: AppProps) {
+  //session={session}
   return (
     <SessionProvider>
-      <SWRConfig 
-        value={{
-          fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-        }}
-      >
-        <AuthProvider >
-          <CartProvider>
-            <UiProvider>
-              <ThemeProvider theme={ lightTheme }>
-                <CssBaseline />
-                <Component {...pageProps} />
-              </ThemeProvider>
-            </UiProvider>
-          </CartProvider>
-        </AuthProvider>
-      </SWRConfig>
+      <PayPalScriptProvider options={{ "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || '' }}>
+        <SWRConfig
+          value={{
+            fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+          }}
+        >
+          <AuthProvider>
+            <CartProvider>
+              <UiProvider>
+                <ThemeProvider theme={lightTheme}>
+                  <CssBaseline />
+                  <Component {...pageProps} />
+                </ThemeProvider>
+              </UiProvider>
+            </CartProvider>
+          </AuthProvider>
+        </SWRConfig>
+      </PayPalScriptProvider>
+
     </SessionProvider>
+
   )
 }
