5c03a8e43b38b84659e201e78b492379807faf80
Jair <jairaceves56@gmail.com>
2023-06-14 10:14:15 -0600

    Cambiamos el Summary para hacerlo responsive

--- components/admin/SummaryTile.tsx
+++ components/admin/SummaryTile.tsx
@@ -1,25 +1,28 @@
-import { Card, CardContent, Grid, Typography } from "@mui/material";
+import { FC } from 'react';
+
+import { Grid, Card, CardContent, Typography } from '@mui/material';
 
 interface Props {
-  title: string | number;
-  subTitle: string;
-  icon: JSX.Element
+    title: string | number;
+    subTitle: string;
+    icon: JSX.Element
 }
-export const SummaryTile = ({ title, subTitle, icon }: Props): JSX.Element => {
+
+
+export const SummaryTile:FC<Props> = ({ title, subTitle, icon }) => {
   return (
-    <Grid container spacing={2}>
+    <Grid item xs={12} sm={4} md={3}>
         <Card sx={{ display: 'flex' }}>
-          <Grid item xs={12} sm={4} md={3}>
-            <CardContent sx={{ width: 50, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-              {/* <CreditCardOutlined color='secondary' sx={{ fontSize: 40 }} /> */}
-              { icon }
+            <CardContent sx={{ width: 50, display:'flex', justifyContent: 'center', alignItems: 'center' }}>
+                {/* <CreditCardOffOutlined color="secondary" sx={{ fontSize: 40 }} /> */}
+                { icon }
             </CardContent>
             <CardContent sx={{ flex: '1 0 auto', display: 'flex', flexDirection: 'column' }}>
-              <Typography variant='h3'>{ title }</Typography>
-              <Typography variant='caption'>{ subTitle }</Typography>
+                <Typography variant='h3'>{ title }</Typography>
+                <Typography variant='caption'>{ subTitle }</Typography>
             </CardContent>
-          </Grid>
         </Card>
-      </Grid>
+        
+    </Grid>
   )
 }
