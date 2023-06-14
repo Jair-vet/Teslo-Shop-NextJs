a3c51f5ee3735b1f76f594beb7449b44897102fd
Jair <jairaceves56@gmail.com>
2023-06-14 09:03:19 -0600

    Cambiamos la interfaz del Usuario para manipular el rol

--- interfaces/user.ts
+++ interfaces/user.ts
@@ -1,10 +1,13 @@
-export interface IUser {
-    _id      : string;
-    name     : string;
-    email    : string;
-    password?: string;
-    role     : string;
+import { Types } from "mongoose";
 
-    createdAt?: string;
-    updatedAt?: string;
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  role: string;
+  user?: IUser;
+  _id?: Types.ObjectId | string;
+
+  createdAt?: string;
+  updatedAt?:string;
 }
