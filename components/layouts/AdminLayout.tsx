8c4673e7eadcbf98902dfa208f76da31b580e73f
Jair <jairaceves56@gmail.com>
2023-06-14 18:06:39 -0600

    Creamos el AdminLayout

--- components/layouts/AdminLayout.tsx
+++ components/layouts/AdminLayout.tsx
@@ -28,7 +28,7 @@ export const AdminLayout = ({ children, title, subTitle, icon }: Props): JSX.Ele
             <Box display='flex' flexDirection='column'>
                 <Typography variant="h1" component="h1">
                     {icon}
-                    {/* {" "} */}
+                    {" "}
                     {title}
                 </Typography>
                 <Typography variant="h2" sx={{ mb: 1 }}>{ subTitle }</Typography>
