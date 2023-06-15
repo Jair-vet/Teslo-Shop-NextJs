4d4c672b087053ff0d3a745ff0ec60d896bd9e5d
Jair <jairaceves56@gmail.com>
2023-06-14 18:12:13 -0600

    Actualizamos el AuthContex en el IUser

--- context/auth/AuthContext.tsx
+++ context/auth/AuthContext.tsx
@@ -1,15 +1,14 @@
+import { IUser } from '@/interfaces';
 import { createContext } from 'react';
-import { IUser } from '../../interfaces';
-
 
 interface ContextProps {
-    isLoggedIn: boolean;
-    user?: IUser;
+  isLoggedIn: boolean;
+  user?: IUser;
 
-    loginUser: (email: string, password: string) => Promise<boolean>;
-    registerUser: (name: string, email: string, password: string) => Promise<{ hasError: boolean; message?: string; }>;
-    logout: () => void;
+  //methods
+  loginUser: (email: string, password: string) => Promise<boolean>
+  logout: () => void
+  registerUser: (name: string, email: string, password: string) => Promise<{ hasError: boolean; message?: string;}>
 }
 
-
-export const AuthContext = createContext({} as ContextProps );
+export const AuthContext = createContext({} as ContextProps)
