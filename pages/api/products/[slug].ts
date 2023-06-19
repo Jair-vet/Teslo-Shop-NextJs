4b0c731bef8184023aea71335700c487d4bc7126
Jair <jairaceves56@gmail.com>
2023-06-19 17:32:36 -0600

    Extraemos las imagenes de los productos desde la API REST

--- pages/api/products/[slug].ts
+++ pages/api/products/[slug].ts
@@ -1,41 +1,36 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { db } from '../../../database';
-import { Product } from '../../../models';
-import { IProduct } from '../../../interfaces';
+import { db } from "@/database";
+import { IProduct } from "@/interfaces";
+import { Product } from "@/models";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = 
-| { message: string }
-| IProduct;
+type Data = { message: string } | IProduct;
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    
-    
-    switch( req.method ) {
-        case 'GET':
-            return getProductBySlug(req, res);
+export default function handler( req: NextApiRequest, res: NextApiResponse<Data>) {
+  switch (req.method) {
+    case "GET":
+      return getProductBySlug(req, res);
 
-        default:
-            return res.status(400).json({
-                message: 'Bad request'
-            })
-    }
-
+    default:
+      return res.status(400).json({ message: "Bad request" });
+  }
 }
 
-async function getProductBySlug(req: NextApiRequest, res: NextApiResponse<Data>) {
+const getProductBySlug = async ( req: NextApiRequest, res: NextApiResponse<Data>) => {
+  
 
-    await db.connect();
-    const { slug } = req.query;
-    const product = await Product.findOne({ slug }).lean();
-    await db.disconnect();
+  await db.connect();
+  const { slug } = req.query;
 
-    if( !product ) {
-        return res.status(404).json({
-            message: 'Producto no encontrado'
-        })
-    }
+  const product = await Product.findOne({slug}).lean();
 
-    return res.json( product );
+  await db.disconnect();
 
+  if(!product) {
+    return res.status(400).json({message: 'Producto no encontrado'})
+  }
+  product.images = product.images.map(image => {
+    return image.includes('http') ? image : `${process.env.HOST_NAME}products/${ image }`
+  })
 
-}
+  return res.status(200).json(product);
+};
